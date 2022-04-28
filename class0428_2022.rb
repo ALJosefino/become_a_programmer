6c8c044e3c6d9c81bcd29c46ba2401091956b67b
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-28 13:18:49 -0300

    I kicked off this ruby course of Danilo Aparecido

---
+++ class0428_2022.rb
@@ -0,0 +1,34 @@
+puts "just a integer number"
+1
+puts "=> to show a content needs puts command"
+puts
+puts "=> puts whats? puts something"
+puts 1
+puts "=> everything in ruby is object, so this is a way to know what class this object there is"
+puts 1.class
+puts "=> after a object .class show class type"
+puts 1.class.superclass
+puts "=> super is same above, Then superclass is above class, it's a superclass"
+puts 1.class.superclass.superclass
+puts "=> These are second level, so it's a subclass"
+puts 1.class.superclass.superclass.superclass
+puts "=> These heritages are show after dot class dot superclass "
+puts 1.2.class
+puts "=> These heritages are show after dot class dot superclass "
+puts 1.2.class.superclass
+
+
+puts ("====================================================================================")
+puts ("Copied from https://en.wikibooks.org/wiki/Ruby_Programming/Data_types")
+puts ("====================================================================================")
+puts "Stringy string McString!".class
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 4.3.class
+puts 4.3.class.superclass
+puts nil.class
+puts h.class
+puts :symbol.class
+puts [].class
+puts (1..8).class
