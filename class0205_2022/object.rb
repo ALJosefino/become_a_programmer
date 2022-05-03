32c0f20cdac147d52183f16912f615aa889b1f89
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-05-03 08:45:47 -0300

    these files were not deleted, just moved to another directory

--- class0205_2022/object.rb
+++
@@ -1,8 +0,0 @@
-#Everything is a object
-puts 1
-puts 1.class
-puts 1.class.superclass
-puts 1.class.superclass.superclass
-puts 1.class.superclass.superclass.superclass
-puts 1.class.superclass.superclass.superclass.superclass
-puts 1.class.superclass.superclass.superclass.superclass.superclass
