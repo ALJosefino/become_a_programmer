be15c8d4ad4337609aa86bc2f86e1db5a1cb5916
André L Josefino <ALJosefino@gmail.com>
2022-05-11 16:14:52 -0300

    loop map and each combianed in Ruby

--- ruby_looping/map2.rb
+++ ruby_looping/map2.rb
@@ -1,2 +1,2 @@
-a = [1,2,3,4,5].map{|x| if x == 4 then " #{x} " end}
+a = [1,2,3,4,5].map{|x| if x == 4 then z = x, "#{z}" end}
       puts a.to_s
---
+++ ruby_looping/map_each.rb
@@ -0,0 +1,4 @@
+a = [1,2,3,4,5].map{|i| "oi - #{i}"}
+      a.each do |i|
+        puts i
+      end
