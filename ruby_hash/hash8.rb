0cfea1e5746292139601174311870e453c92cf62
André L Josefino <ALJosefino@gmail.com>
2022-05-26 11:53:03 -0300

    adding data in a hash in different ways

---
+++ ruby_hash/hash8.rb
@@ -0,0 +1,21 @@
+o = {}
+
+puts o.class
+
+puts ("Adding data in a hash in different ways")
+
+o[:day] = 31
+o[:weekday] = "monday" 
+
+puts o
+
+o["month"] = "march"
+o["season"] = "spring"
+
+puts o
+
+o = {:time => "09:00" }
+
+puts o
+
+puts o[:time]
