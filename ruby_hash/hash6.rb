0358a8e505e65e0ae01c161f7f3b989540e0799c
André L Josefino <ALJosefino@gmail.com>
2022-05-26 11:16:03 -0300

    hashes with string

---
+++ ruby_hash/hash6.rb
@@ -0,0 +1,16 @@
+puts ("hashes with string")
+
+f = {}
+
+puts f.class
+
+f["name"] = "Andre"
+f["mobile"] = "12345678"
+
+puts f.class
+
+puts f
+
+puts f["name"]
+
+puts ("before show with symbol but now with string")
