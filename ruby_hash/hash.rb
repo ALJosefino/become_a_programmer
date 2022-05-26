c543040ea8acbf34b3899366f0e0e2934a91d362
André L Josefino <ALJosefino@gmail.com>
2022-05-26 10:41:46 -0300

    improving hash in ruby language skills

---
+++ ruby_hash/hash.rb
@@ -0,0 +1,13 @@
+a = {}
+
+puts a.class
+
+b = { name:"Andre", middlename:"Luis"}
+
+puts b
+
+c [surname: "Josefino"]
+
+puts c.class
+
+puts c
---
+++ ruby_hash/hash2.rb
@@ -0,0 +1,5 @@
+t = {lastname: "Josefino" mobile: "5551996458560"}
+
+puts t.class
+
+puts t[:lastname]
